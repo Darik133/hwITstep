e63e3998c7f6efb142dc54753a798b2786a8103e
Darii <bunnytt1000@gmail.com>
2026-01-28 16:41:02 +0200

    Add files via upload
    fix mistakes

---
+++ Hw1.py
@@ -0,0 +1,38 @@
+#1
+# name = (input("Write your name  "))
+# age = (input("Write your age  "))
+# print("Hi",name,",""Your age is",age )
+
+#2
+# age = int(input('How old are you?',))
+# if age >= 18:
+#     print('You can vote')
+# else:
+#     print('Go out from here')
+
+#3
+# import random
+# print("guess my number")
+# comp_number = random.randint(1,10)
+# user_number = 0
+# while user_number != comp_number:
+#     user_number = int(input("Enter your guess: "))
+#     if user_number > comp_number:
+#         print("Your guess is too high")
+#     elif user_number < comp_number:
+#         print("Your guess is too low")
+# print("You're guessed")
+
+#7
+# student_mark = int(input("Enter your mark: "))
+#
+# if student_mark < 0 or student_mark > 100:
+#     print("Invalid mark! Please enter a number between 0 and 100.")
+# elif student_mark <= 49:
+#     print("Very bad")
+# elif student_mark <= 69:
+#     print("Nice I think")
+# elif student_mark <= 89:
+#     print("So good")
+# else:
+#     print("Excellent")
---
+++ Hw2.py
@@ -0,0 +1,42 @@
+#1
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#
+#     def get_info(self):
+#         info = f"{self.year} {self.make} {self.model}"
+#         return info
+#
+# car1 = Car("Toyota", "Camry", 2022)
+# car2 = Car("Tesla", "Model S", 2023)
+#
+# print(car1.get_info())
+# print(car2.get_info())
+
+#5
+# class Product:
+#     def __init__(self, name, price, quantity):
+#         self.name = name
+#         self.price = price
+#         self.quantity = quantity
+#
+#     def calculate_total_price(self):
+#         total = self.price * self.quantity
+#         return total
+#
+#     def display_info(self):
+#         print(f"Товар: {self.name}")
+#         print(f"Ціна: {self.price} грн")
+#         print(f"Кількість: {self.quantity}")
+#         print(f"Загальна вартість: {self.calculate_total_price()} грн")
+#         print(f"Повністю: {product1.calculate_total_price()+product2.calculate_total_price()} грн")
+#
+# product1 = Product("Молоко", 35, 3)
+# product2 = Product("Хліб", 25, 2)
+#
+# product1.display_info()
+# print()
+# product2.display_info()
+
