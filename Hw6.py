caa547cdd56afed5a6cf3eb20b1d5aed1134f641
Darii <bunnytt1000@gmail.com>
2026-02-08 16:18:38 +0200

    Add files via upload
    bugs fixed

---
+++ Hw6.py
@@ -0,0 +1,6 @@
+try:
+    user_input = input("Enter a number: ")
+    number = int(user_input)
+    print("You entered an integer:", number)
+except ValueError:
+    print("Error: Bro🐗💀☠️..., the entered number can't be converted to an integer.")
